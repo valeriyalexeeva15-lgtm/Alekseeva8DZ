5edf34824173bddf1376ad9ba6ad5b3477214331
valeriyalexeeva15-lgtm <lera.alekseeva.4240@mail.ru>
2025-10-23 08:04:38 +0300

    Add project files.

---
+++ Alekseeva8DZ/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Alekseeva8DZ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
