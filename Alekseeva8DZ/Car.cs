ef8b97f21c078fa2da4333d07c10e9b00f663def
valeriyalexeeva15-lgtm <lera.alekseeva.4240@mail.ru>
2025-10-23 08:35:17 +0300

    1 задание Car

---
+++ Alekseeva8DZ/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alekseeva8DZ
+{
+    internal class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Марка: {Brand}");
+            Console.WriteLine($"Модель: {Model}");
+            Console.WriteLine($"Год выпуска: {Year}");
+            Console.WriteLine($"Пробег: {Mileage} км");
+            Console.WriteLine();
+        }
+    }
+}
--- Alekseeva8DZ/Program.cs
+++ Alekseeva8DZ/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car car1 = new Car();
+            car1.Brand = "Audi";
+            car1.Model = "A6";
+            car1.Year = 2020;
+            car1.Mileage = 82000;
+
+            Car car2 = new Car();
+            car2.Brand = "Toyota";
+            car2.Model = "Land cruiser";
+            car2.Year = 2023;
+            car2.Mileage = 50000;
+
+            car1.PrintInfo();
+            car2.PrintInfo();
         }
     }
 }
