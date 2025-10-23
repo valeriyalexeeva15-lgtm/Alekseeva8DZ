b7c839272dc39778a42fd32aa9caa0901f7871da
valeriyalexeeva15-lgtm <lera.alekseeva.4240@mail.ru>
2025-10-23 14:28:07 +0300

    5 задание

--- Alekseeva8DZ/Program.cs
+++ Alekseeva8DZ/Program.cs
@@ -2,6 +2,36 @@
 
 namespace MyProject
 {
+    public struct Point
+    {
+        public int X;
+        public int Y;
+    }
+
+    public struct Rectangle
+    {
+        public int Width;
+        public int Height;
+        public Point Position;
+
+        public int CalculateArea()
+        {
+            return Width * Height;
+        }
+
+        public int CalculatePerimeter()
+        {
+            return 2 * (Width + Height);
+        }
+
+        public bool ContainsPoint(Point point)
+        {
+            return point.X >= Position.X &&
+                   point.X <= Position.X + Width &&
+                   point.Y >= Position.Y &&
+                   point.Y <= Position.Y + Height;
+        }
+    }
     public class Program
     {
         static void Main(string[] args)
@@ -41,10 +71,32 @@ namespace MyProject
 
 
             //4
-             global::System.Console.WriteLine("Hello world!");
+            //global::System.Console.WriteLine("Hello world!");
 
-             System mySystem = new System();
-             mySystem.Print();
+            //System mySystem = new System();
+            //mySystem.Print();
+
+            //5
+            Rectangle rect = new Rectangle();
+            rect.Position.X = 10;
+            rect.Position.Y = 10;
+            rect.Width = 20;
+            rect.Height = 15;
+
+            //точки для проверки
+            Point point1 = new Point { X = 15, Y = 15 };
+            Point point2 = new Point { X = 5, Y = 5 };
+            Point point3 = new Point { X = 25, Y = 20 };
+
+            //информация о прямоугольнике
+            Console.WriteLine($"Прямоугольник: позиция ({rect.Position.X},{rect.Position.Y}), ширина {rect.Width}, высота {rect.Height}");
+            Console.WriteLine($"Площадь: {rect.CalculateArea()}");
+            Console.WriteLine($"Периметр: {rect.CalculatePerimeter()}");
+
+            // Проверяем точки
+            Console.WriteLine($"Точка ({point1.X},{point1.Y}) внутри прямоугольника: {rect.ContainsPoint(point1)}");
+            Console.WriteLine($"Точка ({point2.X},{point2.Y}) внутри прямоугольника: {rect.ContainsPoint(point2)}");
+            Console.WriteLine($"Точка ({point3.X},{point3.Y}) внутри прямоугольника: {rect.ContainsPoint(point3)}");
         }   
     }
 }
